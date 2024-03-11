5c37b4f8af7644db76374beee3a83e3d733ddcf0
ricardo-dualboot <ricardo.castillo@dualbootpartners.com>
2024-03-10 23:16:17 -0300

    advancing signals and control flow finished

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter, withViewTransitions } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
@@ -18,12 +17,8 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(),
     provideToastr({ timeOut: 900, preventDuplicates: true }),
     provideStoreDevtools({
-      maxAge: 25, // Retains last 25 states
-      logOnly: !isDevMode(), // Restrict extension to log-only mode
-      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
-      trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
-      traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
-      connectInZone: true // If set to true, the connection is established within the Angular zone
+      maxAge: 25,
+      logOnly: !isDevMode(),
     })
   ],
 };
