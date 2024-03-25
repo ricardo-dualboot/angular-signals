88bb41be1e8db60f27faa9d5a9c9d8cb093bb64f
ricardo-dualboot <ricardo.castillo@dualbootpartners.com>
2024-03-25 10:48:30 -0300

    Implementing computed

--- src/app/features/info/pages/info/info.component.ts
+++ src/app/features/info/pages/info/info.component.ts
@@ -1,5 +1,12 @@
 import { JsonPipe } from '@angular/common';
-import { Component, OnDestroy, computed, effect, inject, signal } from '@angular/core';
+import {
+  Component,
+  OnDestroy,
+  computed,
+  effect,
+  inject,
+  signal,
+} from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { CartStore } from '@shared/store/shopping-cart.store';
 import { ToastrService } from 'ngx-toastr';
@@ -15,20 +22,30 @@ interface Wish {
   standalone: true,
   imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './info.component.html',
-  styles: ``
+  styles: ``,
 })
 export class InfoComponent implements OnDestroy {
   cartStore = inject(CartStore);
-  toastSvc = inject(ToastrService)
+  toastSvc = inject(ToastrService);
   public title = signal<String>('Lista de deseos');
   public wish = new FormControl();
-  public wishes = signal<Wish[]>([])
+  public wishes = signal<Wish[]>([]);
+  public totalWishes = computed(() =>
+    this.wishes().length > 0
+      ? 'Tienes ' +
+        this.wishes().length +
+        (this.wishes().length > 1 ? ' deseos' : ' deseo')
+      : 'No tienes deseos'
+  );
 
   constructor() {
-    this.wishes.set(this.cartStore.wishes())
-    effect(() => {
-      console.log('Effect!', this.wishes());
-    }, { allowSignalWrites: true });
+    this.wishes.set(this.cartStore.wishes());
+    effect(
+      () => {
+        console.log('Effect!', this.wishes());
+      },
+      { allowSignalWrites: true }
+    );
   }
   ngOnDestroy(): void {
     console.log('Effect!', this.wishes());
@@ -36,27 +53,32 @@ export class InfoComponent implements OnDestroy {
   }
 
   addWish = () => {
-    if(this.wish.value && this.wish.value.length > 3 &&
-    !this.wishes().some((wish)=> wish.name.toLowerCase() === this.wish.value.toLowerCase())) {
+    if (
+      this.wish.value &&
+      this.wish.value.length > 3 &&
+      !this.wishes().some(
+        (wish) => wish.name.toLowerCase() === this.wish.value.toLowerCase()
+      )
+    ) {
       this.wishes.update((wishes) => [
         ...wishes,
-        {name: this.wish.value, isCompleted: false, id: this.wishes().length }
+        { name: this.wish.value, isCompleted: false, id: this.wishes().length },
       ]);
-        this.toastSvc.success('Wish added');
+      this.toastSvc.success('Wish added');
     }
     return false;
-  }
+  };
   public resetWishes = () => {
     this.wishes.set([]);
     return false;
-  }
+  };
 
   public markAsComplete = (wish: Wish) => {
     this.wishes.update((wishes: Wish[]) => {
       const wishToUpdate = this.wishes().find((w) => w.name === wish.name);
       if (wishToUpdate) wishToUpdate.isCompleted = true;
       return wishes;
-    })
+    });
     return false;
-  }
+  };
 }
